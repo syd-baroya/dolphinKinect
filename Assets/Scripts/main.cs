14e138d21ce8a3f82fa6d96e1cb06523ea1f6f1a
Sydney T. Baroya <sbaroya@calpoly.edu>
2020-09-10 17:27:05 -0700

    updated dolphin movement

--- Assets/Scripts/main.cs
+++ Assets/Scripts/main.cs
@@ -136,30 +136,6 @@ public class main : MonoBehaviour
 
     }
 
-   
-
-    public static float getAngle(Vector3 fr, Vector3 to, Vector3 up)
-    {
-        // http://answers.unity3d.com/questions/24983/how-to-calculate-the-angle-between-two-vectors.html       
-
-        // the vector perpendicular to referenceForward (90 degrees clockwise)
-        // (used to determine if angle is positive or negative)
-        Vector3 referenceRight = Vector3.Cross(up, fr);
-
-        // Get the angle in degrees between 0 and 180
-        float angle = Vector3.Angle(to, fr);
-
-        // Determine if the degree value should be negative. Here, a positive value
-        // from the dot product means that our vector is the right of the reference vector
-        // whereas a negative value means we're on the left.
-        float sign = (Vector3.Dot(to, referenceRight) > 0.0f) ? 1.0f : -1.0f;
-
-        float finalAngle = sign * angle;
-
-        return finalAngle;
-
-    }
-
        void OnDestroy()
     {
         // Stop background threads.
