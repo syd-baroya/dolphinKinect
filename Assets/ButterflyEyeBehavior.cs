6b3b69df8559f450b6635cd76da15aeb802abc14
Sydney T. Baroya <sbaroya@calpoly.edu>
2020-09-22 17:51:15 -0700

    added new butterfly particle effect

---
+++ Assets/ButterflyEyeBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyEyeBehavior : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //enabled = false;
+        ps = GetComponent<ParticleSystem>();
+        //var main = ps.main;
+        //main.startColor = Random.ColorHSV(0f, 1f, 0f, 1f, 0.5f, 1f, 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ps == null)
+        {
+            enabled = false;
+            return;
+        }
+
+        var main = ps.main;
+        main.startColor = Random.ColorHSV(0f, 1f, 0f, 1f, 0.2f, 1f, 1f, 1f);
+    }
+}
