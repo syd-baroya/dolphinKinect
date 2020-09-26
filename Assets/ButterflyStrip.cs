b12507eb556abf4bacc8ed993f200d0da732a1f9
Sydney T. Baroya <sbaroya@calpoly.edu>
2020-09-26 13:06:36 -0700

    added butterfly strip

---
+++ Assets/ButterflyStrip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyStrip : MonoBehaviour
+{
+    Vector3 middle;
+    public float speed = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        middle = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(GetComponentInChildren<Transform>().transform.position);
+        GetComponentInChildren<Transform>().transform.RotateAround(middle, -Vector3.forward, speed * Time.deltaTime);
+    }
+}
